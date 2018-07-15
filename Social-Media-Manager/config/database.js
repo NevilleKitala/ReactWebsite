162ed6078bc33749108752cff2a0ff71c2c23238
Neville Kitala <nevillekitala@gmail.com>
2018-07-15 16:55:46 +0100

    Major changes to the log in system and basic landing page.

---
+++ Social-Media-Manager/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://nevillekitala:maryland6987@ds137661.mlab.com:37661/filler' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
