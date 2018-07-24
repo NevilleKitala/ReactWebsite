faa1c53405b7d3f1fad98c4dda0c0c6962d9e31c
Neville Kitala <nevillekitala@gmail.com>
2018-07-24 20:15:49 +0300

    Added facebook login

--- Social-Media-Manager/routes.js
+++ Social-Media-Manager/routes.js
@@ -105,6 +105,21 @@ module.exports = function(app, passport) {
               res.render('index');
           });
     });
+
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', {
+      scope : ['public_profile', 'email']
+    }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/home',
+            failureRedirect : '/signup'
+        }));
 };
 
 // route middleware to make sure a user is logged in
