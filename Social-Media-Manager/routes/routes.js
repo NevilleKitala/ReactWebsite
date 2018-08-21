e3128f9c59d25e6c6c12d37d5f2f2ba9d36a5ec7
Neville Kitala <nevillekitala@gmail.com>
2018-08-21 17:13:46 +0300

    Enabled Linking Social Media Accounts

--- Social-Media-Manager/routes/routes.js
+++ Social-Media-Manager/routes/routes.js
@@ -35,7 +35,7 @@ module.exports = function(app, passport) {
     // LOGIN ===============================
     // =====================================
     // show the login form
-    app.get('/login', notLoggedIn, function(req, res) {
+    app.get('/login', function(req, res) {
 
         // render the page and pass in any flash data if it exists
         res.render('login.ejs', { message: req.flash('loginMessage') });
@@ -189,14 +189,4 @@ function isLoggedIn(req, res, next) {
 
     // if they aren't redirect them to the home page
     res.redirect('/');
-};
-
-function notLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
-    if (!req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/home');
 };
